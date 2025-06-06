806139ee39b91673a26cca80a7bddc7a5f1fb96c
shaikmahammadarif36 <shaikmahammadarif36@gmail.com>
2025-06-06 12:56:03 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,7 @@
+my_list=[]
+n=int(input("enter any number:"))
+for x in range(n):
+    num = int(input("enter a number:"))
+    my_list=int(input("enter a number:"))
+    my_list.append(num)
+    print("my updated list is:",my_list)
---
+++ p4
@@ -0,0 +1,5 @@
+my_list=[2,4,6,1,8]
+sum=0
+for i in my_list:
+    sum=sum+i
+    print("the sum is",sum)
